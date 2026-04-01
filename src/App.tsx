b1422ca37f3cb321736e611c0023a569fe2a148c
Jose Escribano <jescribanomora@gmail.com>
2026-04-01 14:48:07 +0200

    feature: create app basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import { Header } from "./components/layout/Header";
-import { Footer } from "./components/layout/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AppProvider } from "./context/AppContext";
+import { Header } from "./components/Layout/Header";
+import { Footer } from "./components/Layout/Footer";
+import { HomePage } from "./pages/HomePage";
+import { ResultsPage } from "./pages/ResultsPage";
 
 function App() {
   return (
-    <>
-      <Header />
-      <div className="bg-amber-400">Hello world!</div>
-      <Footer />
-    </>
+    <BrowserRouter>
+      <AppProvider>
+        <div className="min-h-screen bg-gray-950 text-gray-100 flex flex-col">
+          <Header />
+          <main className="flex-1 max-w-7xl mx-auto w-full px-4 sm:px-6 lg:px-8 py-8">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/results" element={<ResultsPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </AppProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,15 @@
+export function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+      <ul>
+        <li>
+          <a href="/">Homepage</a>
+        </li>
+        <li>
+          <a href="/results">Results</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
