ba2c44ed847a192d5863a142b230124af7fddc12
Jose Escribano <jescribanomora@gmail.com>
2026-04-01 14:23:05 +0200

    feature: add header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Header } from "./components/layout/Header";
+import { Footer } from "./components/layout/Footer";
+
 function App() {
   return (
     <>
+      <Header />
       <div className="bg-amber-400">Hello world!</div>
+      <Footer />
     </>
   );
 }
