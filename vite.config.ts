73136dc511764af4cfb1cc6952e581315b196790
Jose Escribano <jescribanomora@gmail.com>
2026-04-06 18:01:31 +0200

    chore: add new path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,8 @@ export default defineConfig({
       '@components': path.resolve(__dirname, 'src/components'),
       '@context': path.resolve(__dirname, 'src/context'),
       '@pages': path.resolve(__dirname, 'src/pages'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
       '@types': path.resolve(__dirname, 'src/types'),
     },
   },
