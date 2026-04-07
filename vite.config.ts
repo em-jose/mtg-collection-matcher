04ea2da684cdb3958601f542a113a27f347bf404
Jose Escribano <jescribanomora@gmail.com>
2026-04-07 20:29:44 +0200

    feat: Implement example data load button

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
       '@services': path.resolve(__dirname, 'src/services'),
       '@utils': path.resolve(__dirname, 'src/utils'),
       '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@data': path.resolve(__dirname, 'src/data'),
       '@types': path.resolve(__dirname, 'src/types'),
     },
   },
